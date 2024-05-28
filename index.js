13269d9d477d6f3d63a5cf19dd42d82ae0646546
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-05-28 15:43:44 +0300

    add necessary functions

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-function calculateGradesAndAverage(studentScore){
-
-    const studentScore = {name: "Dennis Doe", math: 80, eng: 65, kis: 48, phy: 67, che: 56, re:89, comp: 82};
+function grade_sciences(science_grades){
+    if (science_grades >= 70 && science_grades<= 100) return ('A')
+    if (science_grades>=60 && science_grades<= 69) return ('B')
+    if (science_grades>=50 && science_grades<= 59) return ('C')  
+    if (science_grades>=40 && science_grades<= 49) return ('Pass') 
+    if (science_grades<=39 ) return ('Sup') 
     
+}
+
+
+function grade_religious_education(re_grades){
+    if(re_grades >= 80 && re_grades <=100) return("A")
+    if(re_grades >= 70 && re_grades <=79) return("B")
+    if(re_grades >= 60 && re_grades <=69) return("C")
+    if(re_grades >= 50 && re_grades <=59) return("Pass")
+    if(re_grades < 50 ) return("Sup")
+
+}
+
+function grade_languages(languages_grades){
+    if(languages_grades >=75 && languages_grades <=100) return ('A')
+    if(languages_grades >=65 && languages_grades <=74) return ('B')
+    if(languages_grades >=55 && languages_grades <=64) return ('C') 
+    if(languages_grades >=45 && languages_grades <=54) return ('Pass')
+    if(languages_grades <45 ) return ('Sup')      
 }
