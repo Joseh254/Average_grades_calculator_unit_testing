e3975f01208fad50487abb3928c38cde741cb517
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-05-28 15:46:47 +0300

    add comments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// function to grade sciences subjects
 function grade_sciences(science_grades){
     if (science_grades >= 70 && science_grades<= 100) return ('A')
     if (science_grades>=60 && science_grades<= 69) return ('B')
@@ -7,7 +8,7 @@ function grade_sciences(science_grades){
     
 }
 
-
+// function to grade religious education subjects
 function grade_religious_education(re_grades){
     if(re_grades >= 80 && re_grades <=100) return("A")
     if(re_grades >= 70 && re_grades <=79) return("B")
@@ -17,6 +18,7 @@ function grade_religious_education(re_grades){
 
 }
 
+// function to grade langeages grades
 function grade_languages(languages_grades){
     if(languages_grades >=75 && languages_grades <=100) return ('A')
     if(languages_grades >=65 && languages_grades <=74) return ('B')
